f94c5d84d720ead17c2249f545624b48342402c6
Marcin Styczen <test@github.com>
2015-05-05 13:59:32 +0200

    Drugi commit

--- src/main/java/kuchnia/Start.java
+++ src/main/java/kuchnia/Start.java
@@ -6,7 +6,7 @@ package kuchnia;
 public class Start {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        
+        System.out.println("Cosik");
     }
 
 }
