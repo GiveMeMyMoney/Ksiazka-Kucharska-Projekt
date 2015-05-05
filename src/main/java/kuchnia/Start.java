3b0b6b2995be30e1e644016056b30fb75bbbbe52
Marcin Styczen <test@github.com>
2015-05-05 13:40:03 +0200

    Pierwszy commit

---
+++ src/main/java/kuchnia/Start.java
@@ -0,0 +1,12 @@
+package kuchnia;
+
+/**
+ * Created by Marcin on 2015-05-05.
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+
+}
